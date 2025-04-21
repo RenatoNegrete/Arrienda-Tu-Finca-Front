0935463e0b39687ddd2728ceb51101f994bc5b74
renato <negreterenato@gmail.com>
2025-04-21 14:51:34 -0500

    agregar componente landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ShowFincasComponent } from './components/show-fincas/show-fincas.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 export const routes: Routes = [
+    { path: '', component: LandingComponent},
     { path: 'fincas', component: ShowFincasComponent},
     { path: 'login', component: LoginComponent}
 ];
