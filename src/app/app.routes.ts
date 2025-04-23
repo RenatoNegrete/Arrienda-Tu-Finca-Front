a6d1230d65d740bb0aa9479f5461eee4b4f6d489
felo312 <149544556+felo312@users.noreply.github.com>
2025-04-23 17:36:41 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ShowFincasComponent } from './components/show-fincas/show-fincas.compon
 import { LandingComponent } from './components/landing/landing.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { PostloginarrendadorComponent } from './components/postloginarrendador/postloginarrendador.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
 import { PostloginadminComponent } from './components/postloginadmin/postloginadmin.component'; // Asegúrate de importar el componente correcto para el administrador
 
 export const routes: Routes = [
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'fincas', component: ShowFincasComponent},
     { path: 'login', component: LoginComponent},
     { path: 'signup', component: SignupComponent},
+    { path: 'solicitudes', component: SolicitudesComponent },
     {path:'postloginArrendador' ,component: PostloginarrendadorComponent}, // Cambia esto por el nombre correcto de tu componente
     {path:'postloginadmin' ,component: PostloginadminComponent}, // Cambia esto por el nombre correcto de tu componente
 ];
